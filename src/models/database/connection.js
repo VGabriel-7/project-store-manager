07fc0189f00f3b129281b7d8736f78d1cf483e95
Gabriel Vinícius <viniciusgsa99@gmail.com>
2022-10-17 18:13:17 -0300

    Teste de porta no compose.

--- src/models/database/connection.js
+++ src/models/database/connection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
   host: process.env.MYSQL_HOST || 'localhost',
-  user: process.env.MYSQL_USER || 3306,
+  user: process.env.MYSQL_USER || 33061,
   password: process.env.MYSQL_PASSWORD || 'password',
   database: process.env.MYSQL_DATABASE || 'StoreManager',
 });
